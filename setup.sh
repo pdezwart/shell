59ab7d4d0ec2e1f1e43b9618e3547e0bdafbbc48
Pieter de Zwart <pdezwart@eyeota.com>
2018-10-12 22:44:33 -0700

    Adding vimrcAdding wget

--- setup.sh
+++ setup.sh
@@ -3,16 +3,18 @@
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install the standard packages
+brew install tree
+brew install wget
+
+# Install a better vim:
+curl -o "~/.vimrc" "https://raw.githubusercontent.com/amix/vimrc/master/vimrcs/basic.vim"
+
 # Link in files
 ln -f -s bash_rc ~/.bash_rc
 ln -f -s bash_profile ~/.bash_profile
 ln -f -s bash ~/.bash
 ln -f -s vimrc ~/.vimrc
 ln -f -s screenrc ~/.screenrc
-
-# Install the standard packages
-brew install tree
-
-
 
 
